56c3fe26a82b3d03b5a7860cebfc6c982761df47
Luiz Eduardo Raposo <luizeduardoraposo@protonmail.com>
2025-10-04 23:58:45 -0300

    Fix indentation for console log statements in random number check

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const randomNumber = Math.floor(Math.random() * 100) + 1;
 
 // Check if the number is even or odd
 if (randomNumber % 2 === 0) {
-    console.log(`The number ${randomNumber} is even.`);
+  console.log(`The number ${randomNumber} is even.`);
 } else {
-    console.log(`The number ${randomNumber} is odd.`);
+  console.log(`The number ${randomNumber} is odd.`);
 }
